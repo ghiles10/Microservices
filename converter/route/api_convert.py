debe06f8e0f3a374ae4a3f6215a513d5bfe97779
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-07-12 13:41:31 +0000

    ok authentification

--- converter/route/api_convert.py
+++ converter/route/api_convert.py
@@ -3,7 +3,8 @@ sys.path.append(r"./")
 
 print('ok 1')
 
-from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi import FastAPI, HTTPException, UploadFile,Depends
+from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pymongo import MongoClient
 
@@ -16,19 +17,23 @@ from utils.validate_login import login
 app = FastAPI()
 # security = HTTPBasic()
 
+
+
 print('------------------------- bientot dans post /convert --------------------------------' )
 @app.post("/convert")
-async def convert( mp3_file: UploadFile, credentials: dict  ):
+async def convert(mp3_file: UploadFile, credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
 
     """ Convert MP3 to text route and send it to mongoBD database """
 
     # identification dans l'API
-    # data_credentials = dict(credentials)
-    response = login(credentials)
+    data_credentials = dict(credentials)
+    response = login(data_credentials)
+
+    print('------------------------- verfication login --------------------------------' )
 
     # Vérifier les informations d'identification
     if not response.status_code == 200 : 
-        raise response.raise_for_status()
+        raise HTTPException(response.status_code,  'probleme authenification')
 
     #Traiter le fichier MP3
 
@@ -36,25 +41,25 @@ async def convert( mp3_file: UploadFile, credentials: dict  ):
     file_converted = await convert_mp3_to_text(mp3_file) 
 
     #Mongo db connexion
-    client = MongoClient("mongodb://mongodb:27017/")
+    # client = MongoClient("mongodb://mongodb:27017/")
 
-    print('-------------------------------dans mongo----------------------------')
+    # print('-------------------------------dans mongo----------------------------')
 
-    # crée la base de données
-    db_text_mp3 = client["mp3_text"]
+    # # crée la base de données
+    # db_text_mp3 = client["mp3_text"]
 
-    # crée une collection
-    mp3_text_collection = db_text_mp3["mp3_text_collection"]
+    # # crée une collection
+    # mp3_text_collection = db_text_mp3["mp3_text_collection"]
 
-    # # verifier la base de données 
-    db_list = client.list_database_names()
-    if not "mp3_text" in db_list :
-        raise HTTPException(500,  'la base de données na pas été crée')
+    # # # verifier la base de données 
+    # db_list = client.list_database_names()
+    # if not "mp3_text" in db_list :
+    #     raise HTTPException(500,  'la base de données na pas été crée')
     
-    # verifier la collection 
-    col_list = client.list_collection_names()
-    if not "mp3_text_collection" in col_list:
-        raise HTTPException(500,  'la collection na pas été crée')
+    # # verifier la collection 
+    # col_list = client.list_collection_names()
+    # if not "mp3_text_collection" in col_list:
+    #     raise HTTPException(500,  'la collection na pas été crée')
     
     return file_converted
 
